46ba29f83bfb05ca18238f4ac732374635d868d8
timmipetit <timmipetit@gmail.com>
2015-03-12 16:58:19 +0100

    Run vinagre disowned

--- zsh/selenium-debug.zsh
+++ zsh/selenium-debug.zsh
@@ -1,7 +1,7 @@
 selenium-chrome-debug() {
-    vinagre $(docker port $(docker ps | grep node-chrome-debug | grep -o '^[0-9a-f]*') 5900) &
+    vinagre $(docker port $(docker ps | grep node-chrome-debug | grep -o '^[0-9a-f]*') 5900) &!
 }
 
 selenium-firefox-debug() {
-    vinagre $(docker port $(docker ps | grep node-firefox-debug | grep -o '^[0-9a-f]*') 5900) &
+    vinagre $(docker port $(docker ps | grep node-firefox-debug | grep -o '^[0-9a-f]*') 5900) &!
 }
