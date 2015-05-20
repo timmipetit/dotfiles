fcea4fb0df56937de9862df1fb9cc857538c6f93
timmipetit <timmipetit@gmail.com>
2015-05-20 15:23:30 +0200

    start docker containers

--- zsh/selenium-debug.zsh
+++ zsh/selenium-debug.zsh
@@ -5,3 +5,10 @@ selenium-chrome-debug() {
 selenium-firefox-debug() {
     vinagre $(docker port $(docker ps | grep node-firefox-debug | grep -o '^[0-9a-f]*') 5900) &!
 }
+
+docker-selenium() {
+    docker rm $(docker ps -a -q);
+    docker run -d --name selenium-hub -p 4444:4444 selenium/hub:2.45.0;
+    docker run -d -P --link selenium-hub:hub selenium/node-chrome-debug:2.45.0;
+    docker run -d -P --link selenium-hub:hub selenium/node-firefox-debug:2.45.0;
+}
