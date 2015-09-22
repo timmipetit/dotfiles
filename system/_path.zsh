469d0cb6831fd6d6f58932413f7ca5ee8dca8d51
Tijmen Klein <tijmen@recras.nl>
2015-09-22 10:44:31 +0200

    export gnubin

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,6 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+if $(gls &>/dev/null)
+then
+	export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+fi
